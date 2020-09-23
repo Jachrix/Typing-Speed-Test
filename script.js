5a7c9004768f6d5c655d63cc0132ff82ae2f6448
Anyim Obinna <obinnanyim@gmail.com>
2020-09-23 13:08:57 +0100

    Completing the spell check application

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+
 var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
@@ -30,22 +33,41 @@ function runTimer() {
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    console.log(textEntered);
+    let originTextMatch = originText.substring(0, textEntered.length);
+
+    if (textEntered == originText) {
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#008000";
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCF3";
+        } else {
+            testWrapper.style.borderColor = "#F95D0F";
+        }
+    }
 }
 
 
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
-    if (textEnteredLength === 0) {
-        setInterval(runTimer, 10);
+    if (textEnteredLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
     }
     console.log(textEnteredLength);
 }
 
 // Reset everything:
 function reset() {
-    console.log("Reset button was clicked....");
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timerRunning = false;
+
+    testArea.value = '';
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset button:
